3595d81d31f87293f2f578c512a85800305a0550
minak0429 <20220119@duksung.ac.kr>
2024-10-23 18:30:36 +0900

    삼각형 그리기 1 (색깔 지정X)

--- sketch.js
+++ sketch.js
@@ -7,4 +7,34 @@ function setup() {
 
 function draw() {
   image(img, 0, 0, 1280, 720);
+  
+  noStroke();
+  
+  fill(255,0,0);
+  triangle(584, 149, 673, 118, 676, 164);
+    
+  fill(0,255,0);
+  triangle(673, 118, 676, 164, 738, 127);
+    
+  fill(0,0,255);
+  triangle(738, 127, 676, 164, 757, 229);
+  
+  fill(225,0,0);
+  triangle(676, 164, 757, 229, 654, 229);
+  
+  fill(0,255,0);
+  triangle(591, 209, 676, 164, 654, 229);
+  
+  fill(0,0,255);
+  triangle(584, 149, 676, 164, 591, 209);
+    
+
+  
+  
+  
+  if (mouseIsPressed === true) {
+    console.log(mouseX, mouseY);
+    
+    
+  }
 }
